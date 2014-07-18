67615834dc0f4b2490785eaa94607e441f9e954f
Eric Pascual <eric.pascual@cstb.fr>
2014-07-18 16:22:07 +0200

    logging initialization moved to log module

--- lib/python/pycstbox/__init__.py
+++ lib/python/pycstbox/__init__.py
@@ -15,5 +15,5 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with CSTBox.  If not, see <http://www.gnu.org/licenses/>.
 
-import pycstbox.log
-pycstbox.log.setup_logging()
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
